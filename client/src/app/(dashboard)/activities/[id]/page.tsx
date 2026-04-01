99615e54349d879697e36b70515af5c501fbb19d
Julien Petit <g7029b6d@duck.com>
2026-04-01 22:33:06 +0200

    good units on activity page

--- client/src/app/(dashboard)/activities/[id]/page.tsx
+++ client/src/app/(dashboard)/activities/[id]/page.tsx
@@ -2,6 +2,7 @@ import { fetchActivity } from "@/lib/data";
 import ActivityDetailClient from "@/app/ui/activity/activityDetailClient";
 import BackButton from "@/app/ui/dashboard/backButton";
 import ActivityName from "@/app/ui/activity/activityName";
+import { formatDuration } from "@/lib/utils";
 
 export default async function ActivityPage({
   params,
@@ -17,13 +18,13 @@ export default async function ActivityPage({
       <ActivityName activity={activity} />
       <p>{activity.date}</p>
       <ul>
-        <li>Durée : {Math.round(activity.duration / 60)} min</li>
+        <li>Durée : {formatDuration(activity.duration, false)}</li>
         <li>Distance : {(activity.distance / 1000).toFixed(1)} km</li>
-        <li>Dénivelé : {Math.round(activity.elevGain)} m</li>
         <li>Vitesse max : {activity.maxSpeed.toFixed(1)} km/h</li>
         <li>
           Vitesse moy :{" "}
-          {(activity.distance / (activity.duration / 3600)).toFixed(1)} km/h
+          {(activity.distance / 1000 / (activity.duration / 3600)).toFixed(1)}{" "}
+          km/h
         </li>
       </ul>
       {activity.points && activity.points.length > 0 && (
