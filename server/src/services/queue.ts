a88b0e2f19f35f68a106ab2846f498d495be76de
Julien Petit <g7029b6d@duck.com>
2026-05-05 23:56:30 +0200

    up strava limit

--- server/src/services/queue.ts
+++ server/src/services/queue.ts
@@ -3,7 +3,7 @@ const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export async function processQueue<T>(
   queue: T[],
   processFn: (item: T, index: number) => Promise<void>,
-  delayMs: number = 700,
+  delayMs: number = 300,
 ) {
   for (const [index, item] of queue.entries()) {
     await processFn(item, index);
